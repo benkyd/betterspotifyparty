ff8e22d70c67076b30a3dca34605d4791e913f3a
Jack Dunning <67054303+dunningjack@users.noreply.github.com>
2020-08-26 18:19:53 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import os
+import json
+
+os.environ['SPOTIPY_CLIENT_ID'] = '1071f72324a14278af0998dad4eda1f8'  # from my dash
+os.environ['SPOTIPY_CLIENT_SECRET'] = 'fd48fe6b208646e0abad74fd5d177217'  # from my dash
+os.environ['SPOTIPY_REDIRECT_URI'] = 'https://localhost/api/auth/'
+scope = "user-library-read user-read-currently-playing user-modify-playback-state user-read-playback-state"
+
+OAuth = SpotifyOAuth(scope=scope,
+                     redirect_uri='https://localhost/api/auth/',
+                     cache_path='../cache.txt')
+token = OAuth.get_cached_token()
+sp = spotipy.Spotify(auth_manager=OAuth)
+
+
+def getPlaying():
+    userplaying = sp.current_playback()
+    with open("data_file.json", "w") as write_file:
+        json.dump(userplaying, write_file)
+    with open("data_file.json", "r"):
+        json.loads(userplaying)
+    return userplaying
+
+UserPlaying = getPlaying()
+print(UserPlaying)
