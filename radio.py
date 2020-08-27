fd2ffc69467f9d03319ddec91025a82ba82bacf3
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-08-27 22:48:53 +0100

    Minor changes, seed generation for recommendations started

--- radio.py
+++ radio.py
@@ -3,7 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 os.environ['SPOTIPY_CLIENT_ID'] = '1071f72324a14278af0998dad4eda1f8'  # from my dash
-os.environ['SPOTIPY_CLIENT_SECRET'] = ''  # from my dash
+os.environ['SPOTIPY_CLIENT_SECRET'] = 'c4eb3046229241958dee65273e505f86'  # from my dash
 os.environ['SPOTIPY_REDIRECT_URI'] = 'https://localhost/api/auth/'
 scope = "user-library-read user-read-currently-playing user-modify-playback-state user-read-playback-state"  #
 # provides relevant scopes to auth
@@ -27,19 +27,35 @@ def getPlaying():  # gets the currently playing track for the signed in user
 playingtrackID = getPlaying()
 
 
-def getFeatures():  # calls the API to get the features of a given track ID
-    featuresdata = sp.audio_features(playingtrackID)
-    dance = featuresdata['danceability']
-    energy = featuresdata['energy']
-    valence = featuresdata['valence']
-    tempo = featuresdata['tempo']
-    features = [dance, energy, valence, tempo]
-    return [features]
+#def getFeatures():  # calls the API to get the features of a given track ID
+  #  featuresdata = sp.audio_features(playingtrackID)
+   # dance = featuresdata['danceability']
+  #  energy = featuresdata['energy']
+  #  valence = featuresdata['valence']
+  #  tempo = featuresdata['tempo']
+ #   features = [dance, energy, valence, tempo]
+  #  return [features]
 
 
-audioFeatures = []
-audioFeatures = getFeatures()
+#audioFeatures = []
+#audioFeatures = getFeatures()
+#dance = audioFeatures[0]
+#energy = audioFeatures[1]
+#valence = audioFeatures[2]
+#tempo = audioFeatures[3]
+
 
 userlike = input("Do you like this track? ")
 if userlike == "Yes" or userlike == "yes":
-    analysis = getFeatures()
+    seedtracks = playingtrackID
+
+
+def getRecommended():
+    recommendations = sp.recommendations(seed_tracks=seedtracks)
+    recommendedtracks = recommendations['tracks']['id']
+    return [recommendedtracks]
+
+
+recommmendedtracks = [] = getRecommended()
+
+
