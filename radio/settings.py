a229f317c9914a1c419504823b04fd5a1b266b20
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-08-30 23:13:14 +0100

    changes to environment variables, setup dotenv, boring stuff

--- radio/settings.py
+++ radio/settings.py
@@ -5,7 +5,7 @@ load_dotenv()
 # OR, the same with increased verbosity
 load_dotenv(verbose=True)
 
-# OR, explicitly providing path to 'radio.env'
+# OR, explicitly providing path to '.env'
 from pathlib import Path  # Python 3.6+ only
-env_path = Path('..') / 'radio.env'
+env_path = Path('..') / '.env'
 load_dotenv(dotenv_path=env_path)
