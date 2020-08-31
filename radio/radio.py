bc6a15b759ad9f3d7eba277435c0921734c161e2
dunningjack <67054303+dunningjack@users.noreply.github.com>
2020-08-31 17:30:11 +0100

    now returns recommendation ID for given song. Need to implement recursion and check if response has been given for a playing track

--- radio/radio.py
+++ radio/radio.py
@@ -1,7 +1,9 @@
 import os
+
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
 from dotenv import load_dotenv, find_dotenv
+from spotipy.oauth2 import SpotifyOAuth
+
 load_dotenv(find_dotenv())
 
 SPOTIPY_CLIENT_ID = os.getenv('SPOTIFY_APP_ID')
@@ -23,55 +25,64 @@ sp = spotipy.Spotify(auth_manager=OAuth)
 # Need to implement a callback, taking the assigned token and returning it to the program, rather than entering URL
 # each time for auth
 
-def getPlaying():  # gets the currently playing track for the signed in user
-    userplaying = sp.current_playback()
-    playingdata = userplaying['item']['id']
-    return playingdata
+def checkplaying():
+    playing = sp.current_playback()
+    isplaying = bool(playing["is_playing"])
+    return isplaying
+
+isplaying = checkplaying()
+
+while isplaying:true:
+
+    def getPlaying():  # gets the currently playing track for the signed in user
+        userplaying = sp.current_playback()
+        playingdata = userplaying['item']['id']
+        return playingdata
 
 
-playingtrackID = getPlaying()
+    playingtrackID = getPlaying()
+
+    # def getFeatures():  # calls the API to get the features of a given track ID
+    #  featuresdata = sp.audio_features(playingtrackID)
+    # dance = featuresdata['danceability']
+    #  energy = featuresdata['energy']
+    #  valence = featuresdata['valence']
+    #  tempo = featuresdata['tempo']
+    #   features = [dance, energy, valence, tempo]
+    #  return [features]
 
 
-#def getFeatures():  # calls the API to get the features of a given track ID
-  #  featuresdata = sp.audio_features(playingtrackID)
-   # dance = featuresdata['danceability']
-  #  energy = featuresdata['energy']
-  #  valence = featuresdata['valence']
-  #  tempo = featuresdata['tempo']
- #   features = [dance, energy, valence, tempo]
-  #  return [features]
+    # audioFeatures = []
+    # audioFeatures = getFeatures()
+    # dance = audioFeatures[0]
+    # energy = audioFeatures[1]
+    # valence = audioFeatures[2]
+    # tempo = audioFeatures[3]
+
+    seedtracks = []
+
+    userlike = input("Do you like this track? ")
+    if userlike == "Yes" or userlike == "yes":
+        seedtracks.append(playingtrackID)
 
 
-#audioFeatures = []
-#audioFeatures = getFeatures()
-#dance = audioFeatures[0]
-#energy = audioFeatures[1]
-#valence = audioFeatures[2]
-#tempo = audioFeatures[3]
-
-seedtracks = []
-
-userlike = input("Do you like this track? ")
-if userlike == "Yes" or userlike == "yes":
-    seedtracks.append(playingtrackID)
+    def getRecommended():
+        recommendations = sp.recommendations(seed_tracks=seedtracks)
+        recommendedtracks = recommendations["tracks"][(len(seedtracks))]["id"]
+        return [recommendedtracks]
 
 
-def getRecommended():
-    recommendations = sp.recommendations(seed_tracks=seedtracks)
-    recommendedtracks = recommendations["tracks"][(len(seedtracks))]["id"]
-    return [recommendedtracks]
+    recommmendedtracks = []
+    recommmendedtracks = getRecommended()
 
 
-recommmendedtracks = []
-recommmendedtracks = getRecommended()
+    def recommendedNames():
+        search = sp.tracks(recommmendedtracks)
+        reccnames = search["tracks"][0]["id"]
+        return [reccnames]
 
 
-def recommendedNames():
-    search = sp.tracks(recommmendedtracks)
-    reccnames = search["tracks"][0]["id"]
-    return [reccnames]
+    reccnames = []
+    reccnames.append(recommendedNames())
 
-reccnames =[]
-reccnames.append(recommendedNames())
-
-print(reccnames)
+    print(reccnames)
